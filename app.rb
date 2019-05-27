de48577870c4a3b462d805f50c81246a19a4f67d
Mickae Truman <mtruman@nyit.edu>
2019-05-27 20:33:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,10 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
+ post '/' do
+  text_from_user = params[:user_text]
+ 
+  @analyzed_text = TextAnalyzer.new(text_from_user)
  
   erb :results
 end
